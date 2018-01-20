ede18fbb0e902ba41550b861fcee9467352674eb
Karthick Arunachalam <karthickupgraded@gmail.com>
2018-01-20 16:58:17 +0530

    adding updates

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 // Require express and create an instance of it
 var express = require('express');
-//var bootstrap = require('bootstrap');
 var app = express();
 
+// Middleware to point static files public folder
 app.use(express.static('public'));
 
+app.get("/test", function(req, res){
+  res.sendFile(__dirname+"/public/scrollspy.html");
+});
 
 // Change the 404 message modifing the middleware
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
-
-
 
 // start the server in the port 3000 !
 app.listen(3000, function () {
