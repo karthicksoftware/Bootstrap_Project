1b63a0c62455c378208201849987aaa498d3dfd4
Karthick Arunachalam <karthickupgraded@gmail.com>
2018-01-20 14:25:47 +0530

    adding express static middleware

--- server.js
+++ server.js
@@ -3,16 +3,15 @@ var express = require('express');
 //var bootstrap = require('bootstrap');
 var app = express();
 
-// on the request to root (localhost:3000/)
-app.get('/', function (req, res) {
-    res.sendFile(__dirname+"/index.html");
-});
+app.use(express.static('public'));
 
 
 // Change the 404 message modifing the middleware
 app.use(function(req, res, next) {
     res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
 });
+
+
 
 // start the server in the port 3000 !
 app.listen(3000, function () {
