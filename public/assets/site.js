2c5364e1a43cb5b76dda9d8613e3131634fbf011
Karthick Arunachalam <karthickupgraded@gmail.com>
2018-01-27 15:15:12 +0530

    Updating my project with additional features

--- public/assets/site.js
+++ public/assets/site.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function() {
+      }, 600, function() {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
@@ -22,3 +22,10 @@ $(document).ready(function() {
     } // End if
   });
 });
+
+/*$(document).ready(function() {
+  // Add smooth scrolling to all links
+  $("#more-button").on('click', function(event) {
+    $(elem).hide();
+  });
+});*/
